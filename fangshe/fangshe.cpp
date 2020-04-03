08f78a15ef1595e50b21cd50391177c1aa3bf77a
严浩 <1424412158@qq.com>
2020-04-03 14:46:13 +0800

    3

--- fangshe/fangshe.cpp
+++ fangshe/fangshe.cpp
@@ -2,10 +2,27 @@
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat dstMat;
+	cv::Mat srcMat = cv::imread("C:\\project\\lena.jpg", 1);
+	const cv::Point2f  src_pt[] = {
+									cv::Point2f(200,200),
+									cv::Point2f(250,200),
+									cv::Point2f(200,100)};
+	const cv::Point2f  dst_pt[] = {
+									cv::Point2f(300,100),
+									cv::Point2f(300,50),
+									cv::Point2f(200,100) };
+	const cv::Mat affine_matrix = cv::getAffineTransform(src_pt, dst_pt);
+	cv::warpAffine(srcMat, dstMat, affine_matrix, srcMat.size());
+	cv::imshow("src", srcMat);
+	cv::imshow("dst", dstMat);
+	cv::waitKey(0);
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
